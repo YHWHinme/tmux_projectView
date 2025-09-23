fbefa30432157f2592b9397d4d2e600d44264cde
Archinme <brokenmopflag@gmail.com>
2025-09-23 20:01:06 +0100

    Minor changes
    Changed words sessions to projects

--- tmux.sh
+++ tmux.sh
@@ -2,23 +2,23 @@
 
 CONFIG="/home/oj2/dotfiles/.config/hypr/scripts/deps/sessions.jsonc"
 
-# Extract session names for dmenu
-SESSION=$(jq -r '.sessions[].name' "$CONFIG" | rofi -dmenu -p "Select tmux session:")
+# Extract session names for rofi
+PROJECTS=$(jq -r '.projects[].name' "$CONFIG" | rofi -dmenu -p "Select tmux session:")
 
 # Exit if nothing chosen
-[ -z "$SESSION" ] && exit 0
+[ -z "$PROJECTS" ] && exit 0
 
 # Parse session info
-SESSION_INFO=$(jq -r ".sessions[] | select(.name==\"$SESSION\")" "$CONFIG")
-DIR=$(echo "$SESSION_INFO" | jq -r '.directory')
+PROJECT_INFO=$(jq -r ".projects[] | select(.name==\"$PROJECTS\")" "$CONFIG")
+DIR=$(echo "$PROJECT_INFO" | jq -r '.directory')
 
 # Launch kitty with tmux in the specified directory
 exec kitty bash -c "
   cd $DIR || exit 1
-  if tmux has-session -t \"$SESSION\" 2>/dev/null; then
-    exec tmux attach-session -t \"$SESSION\"
+  if tmux has-session -t \"$PROJECTS\" 2>/dev/null; then
+    exec tmux attach-session -t \"$PROJECTS\"
   else
-    exec tmux new-session -s \"$SESSION\"
+    exec tmux new-session -s \"$PROJECTS\"
   fi
 "
 
